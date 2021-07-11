00005fb1888c1fd7fef7638324ac3782fb168b55
Barun Vishwakarma <barunnitdgp@gmail.com>
2021-07-11 18:11:40 +0530

    Linked list add and print implementation

---
+++ src/main/java/com/linkedlist/Main.java
@@ -0,0 +1,15 @@
+package com.linkedlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList();
+        for(int i=1;i<=100;i++){
+            l.add(i);
+        }
+
+        l.print();
+
+
+    }
+}
