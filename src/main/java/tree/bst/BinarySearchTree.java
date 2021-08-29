64a638e86890e7de3fe45ea258731616876f1421
Barun Vishwakarma <barunnitdgp@gmail.com>
2021-08-29 07:48:42 +0530

    Depth first traversal preorder traversal inorder traversal postorder traversal

--- src/main/java/tree/bst/BinarySearchTree.java
+++ src/main/java/tree/bst/BinarySearchTree.java
@@ -2,7 +2,9 @@ package tree.bst;
 
 import com.util.NumbersUtil;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinarySearchTree {
     public static BSTNode add(BSTNode root, int data) {
@@ -33,20 +35,23 @@ public class BinarySearchTree {
     public static int findMinRecursion(BSTNode root) throws Exception {
         if (root == null) {
             throw new Exception("Tree is empty can not find min");
-        }else if(root.left==null){
+        } else if (root.left == null) {
             return root.data;
         }
         return findMinRecursion(root.left);
 
     }
+
     private static int findMaxRecursion(BSTNode root) throws Exception {
-        if(root==null){
+        if (root == null) {
             throw new Exception("Tree is empty can not find max");
-        }else if(root.right==null){
+        } else if (root.right == null) {
             return root.data;
-        } return findMaxRecursion(root.right);
+        }
+        return findMaxRecursion(root.right);
 
     }
+
     public static int findMin(BSTNode root) throws Exception {
         if (root == null) {
             throw new Exception("Tree is empty can not find min");
@@ -56,40 +61,102 @@ public class BinarySearchTree {
         }
         return root.data;
     }
+
     private static int findMax(BSTNode root) throws Exception {
-        if(root==null){
+        if (root == null) {
             throw new Exception("Tree is empty can not find max");
         }
-        while(root.right!=null){
-            root=root.right;
+        while (root.right != null) {
+            root = root.right;
         }
         return root.data;
     }
 
     private static int findHeight(BSTNode root) {
-        if(root==null){
+        if (root == null) {
             return -1;
-        }
-        else{
-            return Math.max(findHeight(root.left),findHeight(root.right))+1;
+        } else {
+            return Math.max(findHeight(root.left), findHeight(root.right)) + 1;
         }
     }
 
+    public static void levelOrderTraversal(BSTNode root) {
+        if (root == null) {
+            System.out.println("Tree is empty, Can not traverse");
+        } else {
+            Queue<BSTNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                BSTNode node = queue.poll();
+                System.out.print(node.data + " ");
+                if (null!=node.left) {
+                    queue.add(node.left);
+                }
+                if (null!=node.right) {
+                    queue.add(node.right);
+                }
+            }
+        }
+
+    }
+    public static void preorderTraversal(BSTNode root){
+        if(root ==null){
+            return;
+        }else{
+            System.out.print(root.data+" ");
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+    }
+
+    public static void inorderTraversal(BSTNode root){
+        if(root ==null){
+            return;
+        }else{
+            inorderTraversal(root.left);
+            System.out.print(root.data+" ");
+            inorderTraversal(root.right);
+        }
+    }
+
+    public static void postTraversal(BSTNode root){
+        if(root==null){
+            return;
+        }else{
+            postTraversal(root.left);
+            postTraversal(root.right);
+            System.out.print(root.data+" ");
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
         BSTNode rootTemp = null;
-        List<Integer> nums = NumbersUtil.generateRandomList(1, 100, 20);
-        for(Integer n:nums){
-            rootTemp = add(rootTemp,n);
+        List<Integer> nums = NumbersUtil.generateRandomList(1, 100, 10);
+        for (Integer n : nums) {
+            rootTemp = add(rootTemp, n);
         }
 
         BTreePrinter.printNode(rootTemp);
-        System.out.println("Height of tree is: "+findHeight(rootTemp));
+        System.out.println("Height of tree is: " + findHeight(rootTemp));
         search(rootTemp, -100);
         search(rootTemp, -1000);
         System.out.println("Min is :" + findMin(rootTemp));
         System.out.println("Max is :" + findMax(rootTemp));
         System.out.println("Min using recursion :" + findMinRecursion(rootTemp));
         System.out.println("Max using recursion :" + findMaxRecursion(rootTemp));
+        System.out.println("Level Order Traversal");
+        levelOrderTraversal(rootTemp);
+        System.out.println();
+        System.out.println("Preorder Traversal");
+        preorderTraversal(rootTemp);
+        System.out.println();
+        System.out.println("Inorder Traversal");
+        inorderTraversal(rootTemp);
+        System.out.println();
+        System.out.println("Postorder Traversal");
+        postTraversal(rootTemp);
+        System.out.println();
 
     }
 }
